2c69632b5aea97acd91e59d3d5c20063d045bf05
Benjamin Daschel <coffeemaxed@gmail.com>
2015-09-28 11:29:16 -0700

    I think it works

--- assn2.c
+++ assn2.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define QUOTE 34
+#define BACKSLASH 92
+#define NEWLINE 10
+
 #include "prog.h"
 
-void reset_copy(char * copy, char * og, int len){
+//#####
+void reset_copy(char * copy, const char * og, int len){
   strncpy(copy, og, len);
 }
 
@@ -15,18 +20,22 @@ int main(){
     * output the meta part and quote it properly
     * output the bottom of the source
     */
-    char * prog = PROG; //only declare string once for memory efficiency
-    int i, line_count, len = strlen(prog);
+    const char * prog = PROG; //only declare string once for memory efficiency
+    int i, line_count;
+    const int len = strlen(prog);
 
     //make a working copy of the program to tokenize
     char * copy = (char *) malloc(len + 1);
+    char temp [256];
     reset_copy(copy, prog, len);
 
     char * line;
 
     //print one char of the program headers at a time until we reach the marker
-    const char * marker = strstr(copy, "#include \"prog.h\"");
-    char * ptr;
+    // strncpy(temp, , 255); //copy string literal into buffer
+    snprintf(temp, 255, "#include %cprog.h%c", QUOTE, QUOTE); //replace format chars with quotes
+    const char * marker = strstr(copy, temp); //find location of inlcude header
+    char * ptr;  //string index for walking char by char
     for(ptr = copy; ptr < marker; ptr++){
       printf("%c", *ptr);
     }
@@ -39,11 +48,36 @@ int main(){
     );
 
     // reset_copy(copy, prog, len);
-    // // output the definition of PROG as quoted strings
-    // for (i = 0; i < line_count; i++){
-    //   line = strtok(i > 0 ? NULL : copy, "\n");
-    //   printf("\"%s\"\n", line);
-    // }
+    // output the definition of PROG as quoted strings
+    printf("%s", "#define PROG ");
+    reset_copy(copy, prog, len);
+    for (i = 0; i < line_count; i++){
+      line = strtok(i > 0 ? NULL : copy, "\n");
+      printf("%c", QUOTE);
+      //use ascii codes for special chars to avoid meta escaping problem
+      // printf("%c%s%c%c%c", QUOTE, line, QUOTE, BACKSLASH, NEWLINE);
+      for (ptr = line; *ptr != 0; ptr++){
+        switch (*ptr){
+          case QUOTE:
+            printf("%c%c", BACKSLASH, QUOTE);
+            break;
+          case BACKSLASH:
+            printf("%c%c", BACKSLASH, BACKSLASH);
+            break;
+          default:
+            printf("%c", *ptr);
+        }
+      }
+      printf("%c%c", BACKSLASH, 'n'); //print escaped newline char inside string
+      printf("%c%c%c", QUOTE, BACKSLASH, NEWLINE);
+    }
+
+    //output the main body of the program
+    reset_copy(copy, prog, len);
+    ptr = strstr(copy, "#####") - 2;
+    for (; ptr < copy + len; ptr++){
+      printf("%c", *ptr);
+    }
 
     free(copy);
 }
